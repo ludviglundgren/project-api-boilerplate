34e9f657260eb9d5d111f93b67c3497d3c8062a6
aolsson <axel.olsson@chas.se>
2018-04-17 11:03:16 +0200

    Test silly deploy

--- src/routes/index.js
+++ src/routes/index.js
@@ -3,7 +3,7 @@ import C from '../controllers'
 
 export default (app) => {
   app.get('/', (req, res) => res.status(200).send({
-    message: 'Welcome to Node Express API Boilerplate!'
+    message: 'Testing deploy pipeline!'
   }))
 
   /* Sessions */
